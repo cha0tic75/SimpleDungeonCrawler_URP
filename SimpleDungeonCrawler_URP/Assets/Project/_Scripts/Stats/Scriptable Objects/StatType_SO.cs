73ec3524db15814fb01ed685d19350a6fe137b56
Tim McCune <tim.mccune1975@gmail.com>
2022-08-12 13:00:14 -0700

    StatComponents and Damage System both need to be refactored
    See Docs/TODO.txt for more information.

---
+++ SimpleDungeonCrawler_URP/Assets/Project/_Scripts/Stats/Scriptable Objects/StatType_SO.cs
@@ -0,0 +1,13 @@
+// ######################################################################
+// StatType_SO - Script description goes here
+//
+// Written by Tim McCune <tim.mccune1975@gmail.com>
+// ######################################################################
+
+using UnityEngine;
+
+namespace Project.Stats
+{
+	[CreateAssetMenu(menuName = "SO/New Stat Type", fileName = "SO/NewStatType")]
+	public class StatType_SO : ScriptableObject { }
+}
