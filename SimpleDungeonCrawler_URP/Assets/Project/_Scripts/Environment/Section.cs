434e42f306115332783c6b1fb85d6646213a5321
Tim McCune <tim.mccune1975@gmail.com>
2022-08-18 20:04:49 -0700

    Made some builds for different platforms
    Moved all wall tilemaps to a single tilemap and ditched individual ones

--- SimpleDungeonCrawler_URP/Assets/Project/_Scripts/Environment/Section.cs
+++ SimpleDungeonCrawler_URP/Assets/Project/_Scripts/Environment/Section.cs
@@ -7,18 +7,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Project
+namespace Project.Environment
 {
 	[RequireComponent(typeof(Collider2D))]
 	public class Section : MonoBehaviour
 	{
 		#region Inspector Assigned Field(s):
+#if UNITY_EDITOR
+		[SerializeField] private bool m_runOnValidate = false;
+#endif
 		[SerializeField] private int m_sectionNumber;
-		[SerializeField] private bool m_shouldChangeStateOnTrigger = true;
+		[SerializeField] private UnityEngine.Tilemaps.Tilemap m_renderer;
+		[SerializeField] private Color m_groundColor = new Color(1f, 1f, 1f, 1f);
+		[SerializeField] private bool m_shouldNotChangeStateOnTrigger = false;
 		[SerializeField] private List<GameObject> m_activeOnEnterGos;
 		#endregion
 
 		#region MonoBehaviour Callback Method(s):
+#if UNITY_EDITOR
+		private void OnValidate()
+		{
+			if (!m_runOnValidate) { return; }
+			m_renderer.color = m_groundColor;
+			m_renderer.transform.name = $"Ground_{m_sectionNumber}";
+			m_runOnValidate = false;
+		}
+#endif
 		private void Start()
 		{
 			GetComponent<Collider2D>().isTrigger = true;
@@ -31,7 +45,7 @@ namespace Project
 		#region Internally Used Method(s):
 		private void SetActiveState(bool _state)
 		{
-			if (!m_shouldChangeStateOnTrigger) { return; }
+			if (m_shouldNotChangeStateOnTrigger) { return; }
 			m_activeOnEnterGos.ForEach(x => x.SetActive(_state));
 		}
 		#endregion
